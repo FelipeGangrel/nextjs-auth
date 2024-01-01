1fd37834189ffb4c8f480f89d97af817274b1ded
Felipe Gangrel <felipefrog@gmail.com>
2024-01-01 12:28:11 -0300

    Add auth layout

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,9 @@
+import { BaseLayoutProps } from '@/types/common'
+
+export default function AuthLayout({ children }: BaseLayoutProps) {
+  return (
+    <div className="flex h-full items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      {children}
+    </div>
+  )
+}
