85eadc457f639b42fb561fcdead4ebdad44595ba
Felipe Gangrel <felipefrog@gmail.com>
2024-01-01 17:40:17 -0300

    Update register server action

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,7 +1,9 @@
 'use server'
 
+import bcrypt from 'bcrypt'
 import * as z from 'zod'
 
+import { db } from '@/lib/db'
 import { RegisterSchema } from '@/schemas'
 
 export const register = async (data: z.infer<typeof RegisterSchema>) => {
@@ -11,7 +13,31 @@ export const register = async (data: z.infer<typeof RegisterSchema>) => {
     return { error: 'Invalid fields' }
   }
 
+  const { email, name, password } = validatedFields.data
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (existingUser) {
+    return { error: 'Email already in use!' }
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const user = await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  })
+
+  // TODO: Send verification token email
+
   return {
-    success: 'Email sent',
+    success: 'User created!',
   }
 }
