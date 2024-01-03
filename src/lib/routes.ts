169ec3610174500bd7656cedcacedbff9f739803
Felipe Gangrel <felipefrog@gmail.com>
2024-01-03 18:25:20 -0300

    Refactor routes and add API routes

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,6 +1,11 @@
 export const appRoutes = {
   auth: {
-    login: () => '/auth//login' as const,
-    register: () => '/auth//register' as const,
+    login: () => '/auth/login' as const,
+    register: () => '/auth/register' as const,
   },
+  settings: () => '/settings' as const,
+}
+
+export const apiRoutes = {
+  auth: () => '/api/auth' as const,
 }
