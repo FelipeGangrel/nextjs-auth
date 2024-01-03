3584f88b1cc5f3d5b428dfc58a65de9362c39bbf
Felipe Gangrel <felipefrog@gmail.com>
2024-01-03 19:55:21 -0300

    Update bcrypt import to bcryptjs

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,10 +1,9 @@
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import type { NextAuthConfig } from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
 
+import { getUserByEmail } from '@/data/user'
 import { LoginSchema } from '@/schemas/'
-
-import { getUserByEmail } from './data/user'
 
 export default {
   providers: [
