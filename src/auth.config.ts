9a76e117c629bcd457419ca7bd17379245a16994
Felipe Gangrel <felipefrog@gmail.com>
2024-01-04 20:09:24 -0300

    Add comment explaining the usage of Prisma ORM in the authorize function

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -9,6 +9,13 @@ export default {
   providers: [
     Credentials({
       name: 'Credentials',
+      /**
+       * Its okay to use Prisma ORM inside the authorize function because
+       * it will only run on the server, not the edge (serverless).
+       *
+       * We cannot say the same for the callback functions,
+       * which is why we moved them to the auth.ts file.
+       */
       authorize: async (credentials) => {
         const validatedFields = LoginSchema.safeParse(credentials)
 
