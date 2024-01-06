6ab06e20afde53f9321463b3b0ee05a9ab45ddd6
Felipe Gangrel <felipefrog@gmail.com>
2024-01-06 19:19:27 -0300

    Refactor routes.ts to handle optional token parameter

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -3,11 +3,15 @@ export const appRoutes = {
     login: () => '/auth/login' as const,
     register: () => '/auth/register' as const,
     error: () => '/auth/error' as const,
-    newVerification: (token: string) => {
+    newVerification: (token?: string) => {
+      if (!token) return `/auth/new-verification` as const
+
       return `/auth/new-verification?token=${token}` as const
     },
     resetPassword: () => `/auth/reset-password` as const,
-    newPassword: (token: string) => {
+    newPassword: (token?: string) => {
+      if (!token) return `/auth/new-password` as const
+
       return `/auth/new-password?token=${token}` as const
     },
   },
