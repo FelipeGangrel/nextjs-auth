c7df3a78c3e2fa5c15ade142f50afb66f1f4f3d6
Felipe Gangrel <felipefrog@gmail.com>
2024-01-06 19:20:43 -0300

    Add password reset functionality

--- src/actions/reset-password.ts
+++ src/actions/reset-password.ts
@@ -3,6 +3,8 @@
 import * as z from 'zod'
 
 import { getUserByEmail } from '@/data/user'
+import { sendPasswordResetEmail } from '@/lib/mail'
+import { generatePasswordResetToken } from '@/lib/tokens'
 import { ResetPasswordSchema } from '@/schemas'
 
 export const resetPassword = async (
@@ -21,6 +23,13 @@ export const resetPassword = async (
   if (!existingUser) {
     return { error: 'User not found' }
   }
+
+  const passwordResetToken = await generatePasswordResetToken(email)
+
+  await sendPasswordResetEmail(
+    passwordResetToken.email,
+    passwordResetToken.token
+  )
 
   return { success: 'Reset email sent' }
 }
