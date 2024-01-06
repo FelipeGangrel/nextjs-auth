f8b808c2effb9d88aa7282a42b70498915e29b76
Felipe Gangrel <felipefrog@gmail.com>
2024-01-06 19:03:40 -0300

    Refactor verification tokens functions

--- src/data/verification-token.ts
+++ src/data/verification-token.ts
@@ -2,11 +2,9 @@ import { db } from '@/lib/db'
 
 export const getVerificationTokenByEmail = async (email: string) => {
   try {
-    const verificationToken = await db.verificationToken.findFirst({
+    return db.verificationToken.findFirst({
       where: { email },
     })
-
-    return verificationToken
   } catch {
     return null
   }
@@ -14,11 +12,9 @@ export const getVerificationTokenByEmail = async (email: string) => {
 
 export const getVerificationTokenByToken = async (token: string) => {
   try {
-    const verificationToken = await db.verificationToken.findUnique({
+    return db.verificationToken.findUnique({
       where: { token },
     })
-
-    return verificationToken
   } catch {
     return null
   }
