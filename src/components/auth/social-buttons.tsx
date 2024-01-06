c85cc4ae06478edaafe662b829445a3d3fa1760d
Felipe Gangrel <felipefrog@gmail.com>
2024-01-06 12:32:26 -0300

    Add sign-in functionality to social buttons

--- src/components/auth/social-buttons.tsx
+++ src/components/auth/social-buttons.tsx
@@ -1,17 +1,35 @@
 'use client'
 
+import { signIn } from 'next-auth/react'
 import { FaGithub as GithubIcon } from 'react-icons/fa'
 import { FcGoogle as GoogleIcon } from 'react-icons/fc'
 
 import { Button } from '@/components/ui/button'
+import { appRoutes } from '@/lib/routes'
 
 export const SocialButtons = () => {
+  const handleClick = (provider: 'google' | 'github') => {
+    signIn(provider, {
+      callbackUrl: appRoutes.settings(),
+    })
+  }
+
   return (
     <div className="flex w-full items-center gap-x-2">
-      <Button size="lg" variant="outline" className="w-full" onClick={() => {}}>
+      <Button
+        size="lg"
+        variant="outline"
+        className="w-full"
+        onClick={() => handleClick('google')}
+      >
         <GoogleIcon className="h-5 w-5" />
       </Button>
-      <Button size="lg" variant="outline" className="w-full" onClick={() => {}}>
+      <Button
+        size="lg"
+        variant="outline"
+        className="w-full"
+        onClick={() => handleClick('github')}
+      >
         <GithubIcon className="h-5 w-5" />
       </Button>
     </div>
