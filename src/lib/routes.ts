3d809c637f7b93d00ca2a67d89c62ff695f60dda
Felipe Gangrel <felipefrog@gmail.com>
2024-01-06 19:13:07 -0300

    Update routes for new verification and password reset tokens

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -3,8 +3,13 @@ export const appRoutes = {
     login: () => '/auth/login' as const,
     register: () => '/auth/register' as const,
     error: () => '/auth/error' as const,
-    newVerification: () => '/auth/new-verification' as const,
-    resetPassword: () => '/auth/reset-password' as const,
+    newVerification: (token: string) => {
+      return `/auth/new-verification?token=${token}` as const
+    },
+    resetPassword: () => `/auth/reset-password` as const,
+    newPassword: (token: string) => {
+      return `/auth/new-password?token=${token}` as const
+    },
   },
   settings: () => '/settings' as const,
 }
