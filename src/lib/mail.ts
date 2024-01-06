e3a0dace599897f831ed2c04ca1101f4e8bc984b
Felipe Gangrel <felipefrog@gmail.com>
2024-01-06 19:13:16 -0300

    Refactor email templates and add password reset functionality

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -3,9 +3,10 @@ import { Resend } from 'resend'
 import { appRoutes } from './routes'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
+const baseUrl = 'http://localhost:3000'
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `http://localhost:3000/${appRoutes.auth.newVerification()}?token=${token}`
+  const confirmLink = `${baseUrl}/${appRoutes.auth.newVerification(token)}`
 
   await resend.emails.send({
     from: 'onboarding@resend.dev',
@@ -16,6 +17,24 @@ export const sendVerificationEmail = async (email: string, token: string) => {
       <p>Please verify your email address by clicking on the link below:</p>
       <p>
         <a href="${confirmLink}">${confirmLink}</a>
+      </p>
+    </div>
+    `,
+  })
+}
+
+export const sendPasswordResetEmail = async (email: string, token: string) => {
+  const resetLink = `${baseUrl}/${appRoutes.auth.newPassword(token)}`
+
+  await resend.emails.send({
+    from: 'onboarding@resend.dev',
+    to: email,
+    subject: 'Reset your password',
+    html: `
+    <div>
+      <p>Please reset your password by clicking on the link below:</p>
+      <p>
+        <a href="${resetLink}">${resetLink}</a>
       </p>
     </div>
     `,
