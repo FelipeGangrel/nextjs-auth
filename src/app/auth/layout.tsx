dea8061cf3a22f371967cb14fd4fcfe1f1b5ef0d
Felipe Gangrel <felipefrog@gmail.com>
2024-01-06 19:46:13 -0300

    Refactor gradient styles in auth and page layouts

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -2,7 +2,7 @@ import { BaseLayoutProps } from '@/types'
 
 export default function AuthLayout({ children }: BaseLayoutProps) {
   return (
-    <div className="flex h-full items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+    <div className="default-gradient flex h-full items-center justify-center">
       {children}
     </div>
   )
