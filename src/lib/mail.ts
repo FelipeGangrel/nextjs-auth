24993aac36aea2a6dada1f332b1a1f3357741e01
Felipe Gangrel <felipefrog@gmail.com>
2024-01-06 22:45:49 -0300

    Format code

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -40,6 +40,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
     `,
   })
 }
+
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   await resend.emails.send({
     from: 'onboarding@resend.dev',
