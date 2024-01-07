fef610f8b054e3a93f6abb4975d1714b654a26b8
Felipe Gangrel <felipefrog@gmail.com>
2024-01-06 21:45:49 -0300

    Add two-factor authentication token generation

--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -1,6 +1,8 @@
+import crypto from 'crypto'
 import { v4 as uuidV4 } from 'uuid'
 
 import { getPasswordResetTokenByEmail } from '@/data/password-reset-token'
+import { getTwoFactorTokenByEmail } from '@/data/two-factor-token'
 import { getVerificationTokenByEmail } from '@/data/verification-token'
 
 import { db } from './db'
@@ -39,6 +41,24 @@ export const generatePasswordResetToken = async (email: string) => {
   }
 
   return db.passwordResetToken.create({
+    data: { email, token, expiresAt },
+  })
+}
+
+export const generateTwoFactorToken = async (email: string) => {
+  const token = crypto.randomInt(100_000, 999_999).toString()
+  const expiresAt = new Date(new Date().getTime() + TOKEN_EXPIRY_TIME)
+
+  const existingToken = await getTwoFactorTokenByEmail(email)
+
+  if (existingToken) {
+    return db.twoFactorToken.update({
+      where: { id: existingToken.id },
+      data: { token, expiresAt },
+    })
+  }
+
+  return db.twoFactorToken.create({
     data: { email, token, expiresAt },
   })
 }
