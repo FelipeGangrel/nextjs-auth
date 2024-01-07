5a20818347dbb69d0d0e7b1d624beba60ba9b9f8
Felipe Gangrel <felipefrog@gmail.com>
2024-01-06 21:18:38 -0300

    Add two-factor token data functions

---
+++ src/data/two-factor-token.ts
@@ -0,0 +1,21 @@
+import { db } from '@/lib/db'
+
+export const getTwoFactorTokenByEmail = async (email: string) => {
+  try {
+    return db.twoFactorToken.findFirst({
+      where: { email },
+    })
+  } catch {
+    return null
+  }
+}
+
+export const getTwoFactorTokenByToken = async (token: string) => {
+  try {
+    return db.twoFactorToken.findUnique({
+      where: { token },
+    })
+  } catch {
+    return null
+  }
+}
