c97386ed3b6988ab0b23137c65090ffa51d6059d
Felipe Gangrel <felipefrog@gmail.com>
2024-01-07 20:11:42 -0300

    Update env vars and email functions

--- src/@types/environment.d.ts
+++ src/@types/environment.d.ts
@@ -1,8 +1,6 @@
 namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_APP_URL: string
-    NEXT_PUBLIC_DOMAIN_VERIFIED: string
-    NEXT_PUBLIC_DEFAULT_MAIL_TO: string
     GITHUB_CLIENT_ID: string
     GITHUB_CLIENT_SECRET: string
     GOOGLE_CLIENT_ID: string
--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -2,31 +2,15 @@ import { Resend } from 'resend'
 
 import { appRoutes } from './routes'
 
-const {
-  RESEND_API_KEY,
-  NEXT_PUBLIC_APP_URL,
-  NEXT_PUBLIC_DOMAIN_VERIFIED,
-  NEXT_PUBLIC_DEFAULT_MAIL_TO,
-} = process.env
-
-const resend = new Resend(RESEND_API_KEY)
-
-const domain = NEXT_PUBLIC_APP_URL
-
-const getSafeEmail = (email: string) => {
-  if (NEXT_PUBLIC_DOMAIN_VERIFIED.toLocaleLowerCase() === 'true') {
-    return email
-  }
-
-  return NEXT_PUBLIC_DEFAULT_MAIL_TO
-}
+const resend = new Resend(process.env.RESEND_API_KEY)
+const domain = process.env.NEXT_PUBLIC_APP_URL
 
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/${appRoutes.auth.newVerification(token)}`
 
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
-    to: getSafeEmail(email),
+    from: 'mail@felipeoliveira.pro',
+    to: email,
     subject: 'Confirm your email address',
     html: `
     <div>
@@ -43,8 +27,8 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/${appRoutes.auth.newPassword(token)}`
 
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
-    to: getSafeEmail(email),
+    from: 'mail@felipeoliveira.pro',
+    to: email,
     subject: 'Reset your password',
     html: `
     <div>
@@ -59,8 +43,8 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
-    to: getSafeEmail(email),
+    from: 'mail@felipeoliveira.pro',
+    to: email,
     subject: '2FA code',
     html: `
     <div>
