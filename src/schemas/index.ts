07565cdfcd7f79eb0b652695f7883bfda8452011
Felipe Gangrel <felipefrog@gmail.com>
2024-01-07 14:43:26 -0300

    Update updateSettings schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,3 +1,4 @@
+import { UserRole } from '@prisma/client'
 import * as z from 'zod'
 
 export const LoginSchema = z.object({
@@ -34,6 +35,50 @@ export const NewPasswordSchema = z.object({
   }),
 })
 
-export const UpdateSettingsSchema = z.object({
-  name: z.optional(z.string()),
-})
+export const UpdateSettingsSchema = z
+  .object({
+    name: z.optional(z.string()),
+    email: z.optional(
+      z.string().email({
+        message: 'Please enter a valid email',
+      })
+    ),
+    password: z.optional(
+      z.string().min(6, {
+        message: 'Minimum 6 characters required',
+      })
+    ),
+    newPassword: z.optional(
+      z.string().min(6, {
+        message: 'Minimum 6 characters required',
+      })
+    ),
+    isTwoFactorEnabled: z.optional(z.boolean()),
+    role: z.enum([UserRole.ADMIN, UserRole.USER]),
+  })
+  .refine(
+    (data) => {
+      if (data.password && !data.newPassword) {
+        return false
+      }
+
+      return true
+    },
+    {
+      message: 'New password is required when changing password',
+      path: ['newPassword'],
+    }
+  )
+  .refine(
+    (data) => {
+      if (data.newPassword && !data.password) {
+        return false
+      }
+
+      return true
+    },
+    {
+      message: 'Current password is required when changing password',
+      path: ['password'],
+    }
+  )
